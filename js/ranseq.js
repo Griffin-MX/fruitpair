877c1d40f5936550fc8184fe80820efabd798f42
Junde Yhi <lmy441900@aosc.xyz>
2017-04-15 21:56:09 +0800

    js/ranseq.js: add ransel
    This function is used to select a number randomly from a range.
    
    + a small typo fix

--- js/ranseq.js
+++ js/ranseq.js
@@ -27,9 +27,49 @@ function ranseq(n, start) {
 }
 
 /**
+ * Generating `n` numbers from `start` to `end` without repetition, inclusive.
+ *
+ * This implementation mainly uses a "check list" to guarantee no repete numbers
+ * be produced. However, this is not a good method, since:
+ *
+ * - Specifying a super-wide range (end - start + 1) cost a lot memory;
+ * - When `n` is close to range (end - start + 1) there will be many collisions.
+ *
+ * Use this function only when range is not too big and `n` is far away from it.
+ *
+ * @param {Number} start [in] The lower bound.
+ * @param {Number} end   [in] The upper bound.
+ * @param {Number} n     [in] The number of numbers to be retrieved.
+ * @return {Array} An array containing `n` randomly selected numbers.
+ */
+function ransel(start, end, n) {
+  var range = end - start + 1;
+  var selected = new Array(range);
+  var sel      = new Array(n);
+
+  // Sanity check
+  if (n > range) {
+      throw("ransel: range is " + range + " but " + n + " is going to select. This is incorrect.");
+      return;
+  }
+
+  for (let i = 0; i < n;) {
+    let tmp = getRandomIntInclusive(start,end);
+    if (!selected[tmp-start]) {
+      sel[i] = tmp;
+      selected[tmp-start] = true;
+      ++i;
+    }
+  }
+
+  delete selected;
+  return sel;
+}
+
+/**
  * Getting a random integer between two values, inclusive.
  *
- * Retrived from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#Getting_a_random_integer_between_two_values_inclusive.
+ * Retrieved from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#Getting_a_random_integer_between_two_values_inclusive.
  *
  * @param {Number} min [in] the lower bound.
  * @param {Number} max [in] the upper bound.
